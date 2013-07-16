1b2ebb80e72621c1693d9c48c453e3ff75cc8192
amauragis <andrew@mauragis.com>
2013-07-16 17:12:51 -0400

    Remove erroneous comment.

--- arbfunc.c
+++ arbfunc.c
@@ -1,6 +1,3 @@
-/* fileman.c -- A tiny application which demonstrates how to use the
-   GNU Readline library.  This application interactively allows users
-   to manipulate files and their modes. */
 
 #include <stdio.h>
 #include <stdlib.h>
